d42f8a3da881291c08e857a83d804bf0bc6d1add
ov1n <39676588+ov1n@users.noreply.github.com>
2020-04-09 20:55:57 +0530

    Update index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,5 +2,13 @@ const models = require("../models");
 
 //models
 const Issue = models.Issue.IssueModel;
+const Collaborator = models.Collaborator.IssueModel;
+const Mentor = models.Mentor.IssueModel;
 
-// function to handle requests to the main page
+// function to find all the issues on request.
+const readAllIssues = (req, res, callback) => {
+  // Call the model's built in find function and provide it a
+  // callback to run when the query is complete
+  // The built in find function returns an array of matching objects.
+  Issue.find(callback).lean();
+};
