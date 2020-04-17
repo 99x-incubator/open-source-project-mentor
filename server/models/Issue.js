6ecf2aa1292ce3a5e71457e0887e37e66c638bce
ov1n <39676588+ov1n@users.noreply.github.com>
2020-04-18 01:40:42 +0530

    Update Issue.js

--- server/models/Issue.js
+++ server/models/Issue.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose"); //variable to read
 let IssueModel = {};
 
 const IssueSchema = new mongoose.Schema({
+  //required true means it must be essential
   issue_id: {
     type: Number,
     required: true,
@@ -12,16 +13,22 @@ const IssueSchema = new mongoose.Schema({
 
   Name: {
     type: String,
-    required: true,
+    //required: true,
   },
 
   Description: {
     type: String,
   },
 
-  createdDate: {
+  CreatedDate: {
     type: Date,
     default: Date.now,
+  },
+
+  Status: {
+    //whether it is assigned or not
+    type: String,
+    //required: true,  may become needed needed
   },
 });
 
@@ -32,3 +39,10 @@ IssueSchema.statics.findByName = (name, callback) => {
 
   return IssueModel.findOne(search, callback);
 };
+
+// Create the issue model based on the schema.
+IssueModel = mongoose.model("Issue", IssueSchema);
+
+// exporting the properties
+module.exports.IssueModel = IssueModel;
+module.exports.IssueSchema = IssueSchema;
